1b1e5364b376d91a1307e9304c72d1193ae9586a
CebrailDevOps1 <noobione-noobiwan@outlook.com>
2023-07-27 15:25:39 +0200

    edit file copyfiles.sh

--- scripts/copyfiles.sh
+++ scripts/copyfiles.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 /usr/bin/rsync -a -e "ssh -i /home/mysonet/.ssh/id_rsa" mysonet@192.168.122.151:/home/mysonet/mysonet-data/ /home/mysonet/mysonet-data/
 /usr/bin/rsync -a -e "ssh -i /home/mysonet/.ssh/id_rsa" mysonet@192.168.122.151:/home/mysonet/sshkey/ /home/mysonet/sshkey/
-/usr/bin/rsync -a -e "ssh -i /home/mysonet/.ssh/id_rsa" mysonet@192.168.122.151:/home/mysonet/scripts/ /home/mysonet/scripts/
-/usr/bin/chmod -R +x /home/mysonet/scripts/
+
