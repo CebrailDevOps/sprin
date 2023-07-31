ba48aa5d36adcfba8ee49c7ecce0deaed72d24a6
CebrailDevOps1 <noobione-noobiwan@outlook.com>
2023-07-31 05:50:55 +0200

    add file reponse.php

---
+++ mysonet-data/reponse.php
@@ -0,0 +1,22 @@
+<?php
+if (isset($_GET['ref_demande'])) {
+    $ref_demande = $_GET['ref_demande'];
+
+    try {
+        $conn = new PDO('mysql:host=db;dbname=mysonet', 'mysonet', '123456a.');
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // Supprimer la demande d'ami
+        $stmt = $conn->prepare("DELETE FROM demandes_ami WHERE ref_demande = :ref_demande");
+        $stmt->bindParam(':ref_demande', $ref_demande);
+        $stmt->execute();
+
+        if (isset($_GET['ip_add'])) {
+            $ip_add = $_GET['ip_add'];
+            header('Location: http://'.$ip_add.'/notif.php';
+        }
+    } catch(PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+    }
+}
+?>
